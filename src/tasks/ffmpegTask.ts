3395ecb3111333f4d012e061919b208950b4d804
mloetkemann <mloetkemann@users.noreply.github.com>
2023-12-02 11:39:55 +0100

    feat: add output stream reader (#6)

--- src/tasks/ffmpegTask.ts
+++ src/tasks/ffmpegTask.ts
@@ -1,9 +1,46 @@
-import NodeProcessTaskBase from './processTaskBase'
+import { Step, TaskConfig } from 'prostep-js'
+import NodeProcessTaskBase, { OutputStream } from './processTaskBase'
 
 export default class MediaTaskBase extends NodeProcessTaskBase {
   protected inputFile: string | undefined
   protected outputFile: string | undefined
   protected file = 'ffmpeg'
+
+  private duration: number | undefined
+  private reDuration =
+    /Duration: (?<duration>(?<hours>\d\d):(?<minutes>\d\d):(?<seconds>\d\d).(?<milliseconds>\d\d))/i
+  private reTime =
+    /time=(?<duration>(?<hours>\d\d):(?<minutes>\d\d):(?<seconds>\d\d).(?<milliseconds>\d\d))/i
+
+  private getDuration(groups: any): number | undefined {
+    let duration
+    if (groups) {
+      duration =
+        parseInt(groups.seconds) +
+        parseInt(groups.minutes) * 60 +
+        parseInt(groups.hours) * 3600
+    }
+    return duration
+  }
+
+  private outputstreamCallback(str: string) {
+    let groups = str.match(this.reDuration)?.groups
+    const duration = this.getDuration(groups)
+    if (duration) this.duration = duration
+
+    groups = str.match(this.reTime)?.groups
+    const time = this.getDuration(groups)
+    if (time && this.duration) {
+      const progress = Math.round((time / this.duration) * 100)
+      this.logger.info(`Progress ${progress}%`)
+    }
+  }
+
+  constructor(stepConfig: Step, taskConfig: TaskConfig) {
+    super(stepConfig, taskConfig)
+
+    this.stdErrStream = new OutputStream(x => this.outputstreamCallback(x))
+  }
 
   protected async runFFMPEGNode(command: string[]): Promise<string> {
     const result = await this.runNodeProcess(this.file, command)
--- src/tasks/processTaskBase.ts
+++ src/tasks/processTaskBase.ts
@@ -1,8 +1,27 @@
 import { FieldMetaData, InputMetadata, TaskBase } from 'prostep-js'
 import { writeFile } from 'fs/promises'
 import { ExecutableRuntimeContext } from 'prostep-js/dist/lib/base'
+import Stream from 'stream'
+
+export class OutputStream extends Stream.Writable {
+  constructor(private callback: (str: string) => void) {
+    super()
+  }
+
+  _write(
+    chunk: any,
+    encoding: BufferEncoding,
+    next: (error?: Error | null | undefined) => void
+  ) {
+    this.callback(chunk.toString())
+    next()
+  }
+}
 
 export default class NodeProcessTaskBase extends TaskBase {
+  protected stdErrStream: OutputStream | undefined
+  protected sdtoutStream: OutputStream | undefined
+
   getInputMetadata(): InputMetadata {
     return {
       fields: new Array<FieldMetaData>(),
@@ -20,7 +39,23 @@ export default class NodeProcessTaskBase extends TaskBase {
       >)
       ;({ execa } = mod)
     }
-    const { stdout, stderr } = await execa(command, args)
+
+    const promise = execa(command, args, {
+      encoding: 'buffer',
+    })
+
+    if (this.stdErrStream) {
+      const pipeStderr = promise.pipeStderr
+      if (pipeStderr) pipeStderr(this.stdErrStream)
+    }
+
+    if (this.sdtoutStream) {
+      const pipeStdout = promise.pipeStdout
+      if (pipeStdout) pipeStdout(this.sdtoutStream)
+    }
+
+    const { stdout, stderr } = await promise
+
     this.logger.verbose('stdout:')
     this.logger.verbose(stdout.toString())
     this.logger.verbose('stderr:')
